b4eb476a24acfbb1e78b578e0023fc3add31e3be
Dmitriy Shabanov <shabanovd@gmail.com>
2022-05-25 10:49:37 +0500

    use Snapshot for reads and WriteBatch for writes

--- src/rocksdb.rs
+++ src/rocksdb.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-use rocksdb::{DB, Options};
+use rocksdb::{DB, Options, WriteBatch};
 use crate::error::DBError;
 use crate::Memory;
 use crate::memory::{Change, ID, IDS, Record, Value};
@@ -19,7 +19,7 @@ impl Memory for RocksDB {
         options.create_missing_column_families(true);
 
         // list existing ColumnFamilies
-        let cfs = rocksdb::DB::list_cf(&options, path).unwrap_or(vec![]);
+        let cfs = DB::list_cf(&options, path).unwrap_or(vec![]);
         let cf_exist = cfs.iter().find(|cf| cf == &CF_CORE).is_none();
 
         // open DB
@@ -37,15 +37,19 @@ impl Memory for RocksDB {
     fn modify(&self, mutations: Vec<Change>) -> Result<(), DBError> {
         let cf = self.db.cf_handle(CF_CORE).unwrap();
 
+        let mut batch = WriteBatch::default();
         for change in mutations {
             let k = ID::bytes(&change.primary, &change.relation);
+            // TODO let b = change.before.to_bytes()?;
             let v = change.after.to_bytes()?;
 
             debug!("put {:?} = {:?}", k, v);
 
-            self.db.put_cf(cf, k, v)?;
+            batch.put_cf(cf, k, v);
         }
-        Ok(())
+
+        self.db.write(batch)
+            .map_err(|e| e.to_string().into())
     }
 
     fn query(&self, keys: Vec<IDS>) -> Result<Vec<Record>, DBError> {
@@ -53,9 +57,10 @@ impl Memory for RocksDB {
 
         let mut result = Vec::with_capacity(keys.len());
 
+        let pit = self.db.snapshot();
         for key in keys {
             let k = key.to_bytes();
-            let v = self.db.get_cf(cf, &k)?;
+            let v = pit.get_cf(cf, &k)?;
 
             debug!("get {:?} = {:?}", k, v);
 
