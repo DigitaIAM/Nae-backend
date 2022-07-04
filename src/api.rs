139024ad3209b48f84fc40fc7d65e0377d7261f9
Dmitriy Shabanov <shabanovd@gmail.com>
2022-07-04 15:27:44 +0500

    fix todo

--- src/api.rs
+++ src/api.rs
@@ -11,14 +11,13 @@ pub(crate) async fn not_implemented() -> impl Responder {
 #[post("/memory/query")]
 pub(crate) async fn memory_query(db: web::Data<AnimoDB>, keys: web::Json<Vec<TransformationKey>>) -> Result<HttpResponse, Error> {
     // use web::block to offload db request
-    let transformations = // TODO web::block(move || {
+    let transformations = web::block(move || {
         db.query(keys.0)
-    // })
-    //     .await?
+    })
+        .await?
         .map_err(actix_web::error::ErrorInternalServerError)?;
 
-    // Ok(HttpResponse::Ok().json(transformations))
-    todo!()
+    Ok(HttpResponse::Ok().json(transformations))
 }
 
 #[post("/memory/modify")]
@@ -26,10 +25,10 @@ pub(crate) async fn memory_modify(
     db: web::Data<AnimoDB>, mutations: web::Json<Vec<ChangeTransformation>>
 ) -> Result<HttpResponse, Error> {
     // use web::block to offload db request
-    // TODO web::block(move || {
+    web::block(move || {
         db.modify(mutations.0)
-    // })
-    //     .await?
+    })
+        .await?
         .map_err(actix_web::error::ErrorInternalServerError)?;
 
     Ok(HttpResponse::Ok().body(""))
