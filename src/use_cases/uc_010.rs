90ec363ac9cafe3b3a6f485177412585d580a066
antonovmike <antonovma@outlook.com>
2023-04-05 16:30:16 +0500

    first

--- src/use_cases/uc_010.rs
+++ src/use_cases/uc_010.rs
@@ -8,27 +8,32 @@ pub(crate) fn report(app: &Application) {
   unimplemented!()
 }
 
-fn load() -> Vec<(usize, String, String)> {
+fn load() -> JsonValue {
   let text_file = "utf8_dbo.GOOD.Table.sql";
   let file = File::open(text_file).unwrap();
 
   let mut search_id = 0;
-  let mut catalog: Vec<(usize, String, String)> = vec![];
+
+  let mut jvalue = JsonValue::new_array();
 
   BufReader::new(file)
-    .lines()
-    .map(|l| l.unwrap())
-    .filter(|l| l.starts_with("INSERT"))
-    .map(|l| l[398..].to_string())
-    .map(|l| {
-      let name = l.split("N'").nth(1).unwrap();
-      let manufacturer = l.split("N'").nth(1).unwrap();
-      (name[0..name.len() - 3].to_owned(), manufacturer[0..manufacturer.len() - 3].to_owned())
-    })
-    .for_each(|(name, manufacturer)| {
-      catalog.push((search_id, name.to_string(), manufacturer.to_string()));
-      search_id += 1;
-    });
+      .lines()
+      .map(|l| l.unwrap())
+      .filter(|l| l.starts_with("INSERT"))
+      .map(|l| l[398..].to_string())
+      .map(|l| {
+          let name = l.split("N'").nth(1).unwrap();
+          let manufacturer = l.split("N'").nth(1).unwrap();
+          let both = format!(
+              "{}; {}",
+              name[0..name.len() - 3].to_owned(),
+              manufacturer[0..manufacturer.len() - 3].to_owned()
+          );
+          jvalue.push(both)
+      })
+      .for_each(|js_obj| {
+          search_id += 1;
+      });
 
-  catalog
+  jvalue
 }
