3b6b7bf9c38c4d28c7dec15c703256dac8415fea
antonovmike <antonovma@outlook.com>
2023-04-07 10:50:05 +0500

    before, after

--- src/text_search/process_text_search.rs
+++ src/text_search/process_text_search.rs
@@ -11,21 +11,9 @@ struct JsonValueObject {
 }
 
 pub fn process_text_search(app: &Application,  ctx: &Vec<String>, before: &JsonValue, data: &JsonValue) {
-  if ctx[0] == "drugs" {
-    let before_str = format!("{}", before);
-    let data_str = format!("{}", data);
-    let bfr: JsonValueObject = serde_json::from_str(&before_str).unwrap();
-    let dta: JsonValueObject = serde_json::from_str(&data_str).unwrap();
-
-    if dta.name.is_empty() && !bfr.name.is_empty() {
-      todo!() // remove bfr.name
-    }
-    if !dta.name.is_empty() && bfr.name.is_empty() {
-      todo!() // add dta.name
-    }
-    if dta.name != bfr.name {
-      todo!() // replace bfr.name with dta.name
-    }
+  if ctx == &vec!["drugs"] {
+    let before_name = before["name"].as_str();
+    let after_name = data["name"].as_str();
 
     todo!()
   }
