65c05c416f4b3018fa33af4827958142141c0040
antonovmike <antonovma@outlook.com>
2023-04-11 13:45:21 +0500

    TantivySearch rewrite

--- src/text_search/search_engines.rs
+++ src/text_search/search_engines.rs
@@ -36,55 +36,20 @@ impl Search for SimSearchEngine {
 
 #[derive(Clone)]
 pub struct TantivySearch {
-  index: Index,
-  // pub engine: SimSearch<Uuid>,
+  // index: Index,
+  pub engine: SimSearch<Uuid>,
 }
 
 impl TantivySearch {
   pub fn new() -> Self {
-      let mut schema_builder = Schema::builder();
-      schema_builder.add_text_field("body", TEXT);
-      schema_builder.add_text_field("id", TEXT | STORED);
-
-      let schema = schema_builder.build();
-
-      let path = "./tantivy";
-
-      let index = Index::create_in_dir(&path, schema).unwrap();
-
-      TantivySearch { index }
+      Self {
+          engine: SimSearch::new(),
+      }
   }
 }
 
 impl Search for TantivySearch {
   fn search(&self, input: &str) -> Vec<Uuid> {
-      let reader = self
-          .index
-          .reader_builder()
-          .reload_policy(ReloadPolicy::OnCommit)
-          .try_into()
-          .unwrap();
-
-      let searcher = reader.searcher();
-
-      let id = self.index.schema().get_field("id").unwrap();
-      let body = self.index.schema().get_field("body").unwrap();
-
-      let parser = QueryParser::for_index(&self.index, vec![ body ]);
-      let query = parser.parse_query(input).unwrap();
-
-      let top_docs = searcher.search(&query, &TopDocs::with_limit(10)).unwrap();
-
-      let mut results = vec![];
-      
-      for (_score, doc_address) in top_docs {
-          let retrieved_doc = searcher.doc(doc_address).unwrap();
-          let id = retrieved_doc.get_first(id).unwrap();
-          let id = id.as_text().unwrap();
-          let id = Uuid::parse_str(id).unwrap();
-          results.push(id);
-      }
-
-      results
+      self.engine.search(input)
   }
 }
