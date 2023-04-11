5d7960d1568d54e568ab74028216c92de9776060
antonovmike <antonovma@outlook.com>
2023-04-11 10:18:59 +0500

    uc_010 report

--- src/use_cases/uc_010.rs
+++ src/use_cases/uc_010.rs
@@ -1,11 +1,11 @@
 use crate::commutator::Application;
-use std::fs::{File, self};
+use std::fs::File;
 use std::io::{BufRead, BufReader};
 use json::{JsonValue, object};
 use service::Services;
 
 use crate::storage::memories::memories_create;
-use crate::text_search::process_text_search;
+// use crate::text_search::process_text_search;
 
 const DRUGS: [&str; 1] = ["drugs"];
 
@@ -21,15 +21,16 @@ pub(crate) fn import(app: &Application) {
 pub(crate) fn report(app: &Application) {
   let oid = "yjmgJUmDo_kn9uxVi8s9Mj9mgGRJISxRt63wT46NyTQ";
   let ctx = DRUGS.to_vec();
+  println!("CTX\t{ctx:?}");
   let result = app.service("memories").find(object! {oid: oid, ctx: ctx, search: "ПЛАКВЕНИЛ"}).unwrap();
-  println!("{}", result.dump());
+  println!("report:\n{}\nend of report", result.dump());
 }
 
 fn load() -> Vec<JsonValue> {
   let text_file = "utf8_dbo.GOOD.Table.sql";
   let file = File::open(text_file).unwrap();
 
-  let mut search_id = 0;
+  // let mut search_id = 0;
 
   BufReader::new(file)
       .lines()
