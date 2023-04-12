d71d9b29a3420cc040d8e1e08ebb019bc4c4f742
antonovmike <antonovma@outlook.com>
2023-04-12 13:09:06 +0500

    Tantivy insert delete search

--- src/text_search/process_text_search.rs
+++ src/text_search/process_text_search.rs
@@ -9,81 +9,82 @@ use uuid::Uuid;
 use crate::{
   commutator::Application, storage::Workspaces, 
   text_search::search_engines::SimSearchEngine,
-  text_search::search_engines::TantivySearch,
+  text_search::search_engines::TantivyEngine,
   text_search::search_engines::Search,
 };
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
-struct JsonValueObject {
-  name: String,
-  manufacturer: String,
-  // id: String,
-  // uuid: String,
-}
-
 pub trait SearchTrait {
-  fn load(&mut self, catalog: Vec<(Uuid, String)>);
+  // fn load(&mut self, catalog: Vec<(Uuid, String)>);
   fn search(&self, input: &str) -> Vec<Uuid>;
 }
 
 #[derive(Clone)]
 pub struct SearchEngine {
-  catalog: Vec<(Uuid, String)>,
-  // engine: SimSearch<Uuid>,
-  engine: TantivySearch,
+  sim: SimSearch<Uuid>,
+  tan: TantivyEngine,
 }
 
 impl SearchEngine {
   pub fn new() -> Self {
     Self { 
-      catalog: vec![], 
-      // engine: SimSearch::new(),
-      // engine: SimSearchEngine::new().engine,
-      engine: TantivySearch::new(),
+      sim: SimSearch::new(),
+      tan: TantivyEngine::new(),
     }
   }
-
+// SIMSEARCH индексирует базу, т.к хранит индекс не на диске, а в памяти.
   pub fn load(&mut self, workspaces: Workspaces) -> Result<(), service::error::Error> {
+// Итерация по всем рабочим пространствам (организациям)
     for ws in workspaces.list()? {
+// Получить все записи из рабочего пространства с контекстом "drugs"
       let memories = ws.memories(vec!["drugs".to_string()]);
-      
+// Итерация по всем записям
       for mem in memories.list(None)? {
+// Получить данные записи в формате JsonValue
         let jdoc = mem.json()?;
+// Получить имя записи и uuid
         let name = jdoc["name"].as_str().unwrap();
         let uuid = jdoc["_uuid"].as_str().unwrap();
+// Преобразовать uuid в тип Uuid
         let uuid = Uuid::parse_str(uuid).unwrap();
-        // self.engine.insert(uuid, name);
-        self.engine.load(vec![(uuid, name.to_string())]);
+// Добавить запись в индекс поискового движка SimSearch
+        self.sim.insert(uuid, name);
       }
     }
+// Теперь у SimSearch есть в памяти индекс, который можно использовать для поиска
 
     Ok(())
   }
-
+// Создать запись. Сейчас она добавляется в каталог, который я создал.
+// А должна добавляться в индекс поисковых движков.
   pub fn create(&mut self, id: Uuid, text: &str) {
-    self.catalog.push((id, text.to_string()));
+    self.sim.insert(id, text);
+    self.tan.insert(id, text);
   }
-
+// Изменить запись. Удалить из индекса поисковых движков и добавить новую версию.
   pub fn change(&mut self, id: Uuid, _before: &str, after: &str) {
     self.delete(&id);
     self.create(id, after);
   }
-
+// Удалить запись. Сейчас она удаляется из каталога, который я создал.
+// А должна удаляться из индекса поисковых движков.
+// Как удалить запись из индекса SimSearch?
   pub fn delete(&mut self, id: &Uuid) {
-    if let Some(index) = self.catalog.iter().position(|(current_id, _current_text)| current_id == id)
-    {
-      self.catalog.remove(index);
-    };
+// Удалить запись из каталога. Заменить на удаление из индекса поисковых движков Tantivy и SimSearch
+    self.sim.delete(id);
+    self.tan.delete(*id);
   }
-
+// Поиск по индексу SimSearch
+// Нужно добавить поиск по индексу Tantivy
   pub fn search(&mut self, text: &str) -> Vec<Uuid> {
     println!("-> {text}");
-    let result = self.engine.search(text);
-    println!("result.len() = {}", result.len());
-    result
+    let mut result_sim = self.sim.search(text);
+    let mut result_tan = self.tan.search(text);
+    result_sim.append(&mut result_tan);
+    println!("result.len() = {}", result_sim.len());
+    result_sim
   }
 }
-
+// Проверка актуальности данных в базе
 pub fn process_text_search(
   app: &Application,
   ctx: &Vec<String>,
@@ -91,25 +92,34 @@ pub fn process_text_search(
   data: &JsonValue,
 ) -> Result<(), Error> {
   // dbg!(&ctx, &before, &data);
+// Проверить, что контекст равен "drugs"
   if ctx == &vec!["drugs"] {
+// Получить uuid записи
     let id = data["_uuid"].as_str().map(|data| Uuid::parse_str(data).unwrap()).unwrap();
+// Получить имя записи до и после изменения
     let before_name = before["name"].as_str();
     let after_name = data["name"].as_str();
-
+// Если имя до изменения не равно имени после изменения, то
     if let Some(before_name) = before_name {
+// Если имя после изменения не пустое, то
       if let Some(after_name) = after_name {
+// Если имя до изменения не равно имени после изменения, то
         if before_name == after_name {
           // IGNORE
         } else {
+// Иначе изменить запись в индексе поисковых движков вызвав метод change у движков поиска
           let mut search = app.search.write().unwrap();
           search.change(id, before_name, after_name);
         }
       } else {
+// Иначе удалить запись из индекса поисковых движков вызвав метод delete поискового движка
         let mut search = app.search.write().unwrap();
         search.delete(&id);
       }
     } else {
+// Если имя после изменения не пустое, то
       if let Some(after_name) = after_name {
+// Иначе создать запись в индексе поисковых движков вызвав метод create у обоих движков
         let mut search = app.search.write().unwrap();
         search.create(id, after_name);
       } else {
--- src/text_search/search_engines.rs
+++ src/text_search/search_engines.rs
@@ -9,7 +9,8 @@ use tantivy::Document;
 use uuid::Uuid;
 
 pub trait Search {
-  fn load(&mut self, catalog: Vec<(Uuid, String)>);
+  fn insert(&mut self, id: Uuid, text: &str);
+  fn delete(&mut self, id: Uuid);
   fn search(&self, input: &str) -> Vec<Uuid>;
 }
 
@@ -31,65 +32,68 @@ impl SimSearchEngine {
 // ------------------------------
 
 #[derive(Clone)]
-pub struct TantivySearch {
+pub struct TantivyEngine {
   // schema: Schema,
   index: Index,
 }
 
-impl TantivySearch {
+impl TantivyEngine {
+// Создаем новый индекс в памяти с полями body и id
   pub fn new() -> Self {
+// Создаем схему индекса с полями body и id
     let mut schema_builder = Schema::builder();
     schema_builder.add_text_field("body", TEXT);
     schema_builder.add_text_field("id", TEXT | STORED);
-
+// Собираем схему индекса в индекс
     let schema = schema_builder.build();
-
-    let index = Index::create_in_ram(schema);
-
+// Создаем индекс в памяти 
+    let directory_path = "./tantivy";
+    let index = Index::create_in_dir(directory_path, schema).unwrap();
+// Возвращаем структуру TantivySearch
     Self {
       index,
     }
   }
 }
 
-impl Search for TantivySearch {
-  fn load(&mut self, catalog: Vec<(Uuid, String)>) {
-      let mut writer = self.index.writer(3_000_000).unwrap();
-
-      let body = self.index.schema().get_field("body").unwrap();
-      let id = self.index.schema().get_field("id").unwrap();
-
-      for (uuid, text) in catalog {
-        let mut rec = Document::default();
-        rec.add_text(body, text);
-        rec.add_text(id, uuid.to_string());
-        writer.add_document(rec).unwrap();
-      }
-
-      writer.commit().unwrap();
+impl Search for TantivyEngine {
+  fn insert(&mut self, id: Uuid, text: &str) {
+    unimplemented!()  
   }
 
+  fn delete(&mut self, id: Uuid) {
+    unimplemented!()
+  }
+  
   fn search(&self, input: &str) -> Vec<Uuid> {
+// Создаем reader для поиска по индексу и searcher для поиска по reader
     let reader = self
       .index
+// Перезагружаем индекс при запуске поиска
       .reader_builder()
+// Перезагружаем индекс при commit()
       .reload_policy(ReloadPolicy::OnCommit)
+// Создаем reader из индекса 
       .try_into()
+// Если reader не создался, то возвращаем пустой вектор Uuid
       .unwrap();
 
     let searcher = reader.searcher();
 
     let id = self.index.schema().get_field("id").unwrap();
     let body = self.index.schema().get_field("body").unwrap();
-
+// Создаем парсер запросов для поиска по полю body
     let parser = QueryParser::for_index(&self.index, vec![ body ]);
+// Парсим запрос
     let query = parser.parse_query(input).unwrap();
-
+// получаем результат поиска по индексу searcher с ограничением в 10 документов
     let top_docs = searcher.search(&query, &TopDocs::with_limit(10)).unwrap();
-
+// Преобразуем результат в вектор из Uuid
     let mut result: Vec<Uuid> = vec![];
     result.extend(top_docs.iter().map(|(_score, doc_address)| {
+// Получаем документ по адресу из результата поиска по индексу searcher
       let retrieved_doc = searcher.doc(*doc_address).unwrap();
+// Получаем поле id из документа и преобразуем его в Uuid
       let id = retrieved_doc.get_first(id).unwrap();
       let id = id.as_text().unwrap();
       Uuid::parse_str(id).unwrap()
