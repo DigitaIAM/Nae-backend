2e7e851af3a8b599be8f6b044931df11bdce059a
antonovmike <antonovma@outlook.com>
2023-04-12 16:38:32 +0500

    insert index_writer.add_document

--- src/text_search/search_engines.rs
+++ src/text_search/search_engines.rs
@@ -5,7 +5,7 @@ use tantivy::schema::{Schema, STORED, TEXT, Value};
 use tantivy::{Index, ReloadPolicy};
 use tantivy::query::QueryParser;
 use tantivy::collector::TopDocs;
-use tantivy::Document;
+use tantivy::{doc, Document};
 use uuid::Uuid;
 
 pub trait Search {
@@ -42,8 +42,8 @@ impl TantivyEngine {
   pub fn new() -> Self {
 // Создаем схему индекса с полями body и id
     let mut schema_builder = Schema::builder();
-    schema_builder.add_text_field("body", TEXT);
-    schema_builder.add_text_field("id", TEXT | STORED);
+    schema_builder.add_text_field("uuid", TEXT | STORED);
+    schema_builder.add_text_field("name", TEXT);
 // Собираем схему индекса в индекс
     let schema = schema_builder.build();
 // Создаем индекс в памяти 
@@ -58,46 +58,49 @@ impl TantivyEngine {
 
 impl Search for TantivyEngine {
   fn insert(&mut self, id: Uuid, text: &str) {
-    unimplemented!()  
+    let mut index_writer = self.index.writer(3_000_000).unwrap();
+
+    let uuid = self.index.schema().get_field("uuid").unwrap();
+    let name = self.index.schema().get_field("name").unwrap();
+
+    index_writer.add_document(doc!{
+      uuid => id.to_string(),
+      name => text,
+    }).unwrap();
+
+    index_writer.commit().unwrap();
   }
 
   fn delete(&mut self, id: Uuid) {
     unimplemented!()
   }
-  
+
   fn search(&self, input: &str) -> Vec<Uuid> {
-// Создаем reader для поиска по индексу и searcher для поиска по reader
     let reader = self
       .index
-// Перезагружаем индекс при запуске поиска
       .reader_builder()
-// Перезагружаем индекс при commit()
       .reload_policy(ReloadPolicy::OnCommit)
-// Создаем reader из индекса 
       .try_into()
-// Если reader не создался, то возвращаем пустой вектор Uuid
       .unwrap();
 
     let searcher = reader.searcher();
 
     let id = self.index.schema().get_field("id").unwrap();
     let body = self.index.schema().get_field("body").unwrap();
-// Создаем парсер запросов для поиска по полю body
+
     let parser = QueryParser::for_index(&self.index, vec![ body ]);
-// Парсим запрос
+
     let query = parser.parse_query(input).unwrap();
 // получаем результат поиска по индексу searcher с ограничением в 10 документов
     let top_docs = searcher.search(&query, &TopDocs::with_limit(10)).unwrap();
 // Преобразуем результат в вектор из Uuid
-    let mut result: Vec<Uuid> = vec![];
-    result.extend(top_docs.iter().map(|(_score, doc_address)| {
+    top_docs.iter().map(|(_score, doc_address)| {
 // Получаем документ по адресу из результата поиска по индексу searcher
       let retrieved_doc = searcher.doc(*doc_address).unwrap();
-// Получаем поле id из документа и преобразуем его в Uuid
+
       let id = retrieved_doc.get_first(id).unwrap();
       let id = id.as_text().unwrap();
       Uuid::parse_str(id).unwrap()
-    }));
-    result
+    }).collect()
   }
 }
