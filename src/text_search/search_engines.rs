aa60ef2aac28c6c971daf2999705fd8c5c91a771
antonovmike <antonovma@outlook.com>
2023-04-17 18:35:26 +0500

    Arc<Mutex<IndexReader>>

--- src/text_search/search_engines.rs
+++ src/text_search/search_engines.rs
@@ -7,7 +7,7 @@ use tantivy::schema::{
   Schema, STORED, TEXT, 
   // Value
 };
-use tantivy::{Index, ReloadPolicy, Term, doc, IndexWriter};
+use tantivy::{Index, ReloadPolicy, Term, doc, IndexWriter, IndexReader};
 use tantivy::query::QueryParser;
 use tantivy::collector::TopDocs;
 // use tantivy::{doc, Document};
@@ -15,6 +15,7 @@ use uuid::Uuid;
 
 pub trait Search {
   fn index_writer(&mut self) -> Arc<Mutex<IndexWriter>>;
+  fn index_reader(&self) -> Arc<Mutex<IndexReader>>;
   fn insert(&mut self, id: Uuid, text: &str);
   fn delete(&mut self, id: Uuid);
   fn search(&self, input: &str) -> Vec<Uuid>;
@@ -45,13 +46,12 @@ pub struct TantivyEngine {
 
 impl TantivyEngine {
   pub fn new() -> Self {
-// Создаем схему индекса с полями body и id
     let mut schema_builder = Schema::builder();
     schema_builder.add_text_field("uuid", TEXT | STORED);
     schema_builder.add_text_field("name", TEXT);
 
     let schema = schema_builder.build();
-// Читаем индекс с диска 
+
     let directory_path = "./tantivy";
     // if Index::exists(directory_path)
     let index = if Path::new(directory_path).is_dir() {
@@ -70,6 +70,10 @@ impl TantivyEngine {
 impl Search for TantivyEngine {
   fn index_writer(&mut self) -> Arc<Mutex<IndexWriter>> {
     Arc::new(Mutex::new(self.index.writer(3_000_000).unwrap()))
+  }
+
+  fn index_reader(&self) -> Arc<Mutex<IndexReader>> {
+    Arc::new(Mutex::new(self.index.reader_builder().reload_policy(ReloadPolicy::OnCommit).try_into().unwrap()))
   }
 
   fn insert(&mut self, id: Uuid, text: &str) {
@@ -96,17 +100,15 @@ impl Search for TantivyEngine {
   }
 
   fn search(&self, input: &str) -> Vec<Uuid> {
+    // let reader = self
+    //   .index
+    //   .reader_builder()
+    //   .reload_policy(ReloadPolicy::OnCommit)
+    //   .try_into()
+    //   .unwrap();
 
-    println!("search_engines.rs FN SEARCH: {input}");
-
-    let reader = self
-      .index
-      .reader_builder()
-      .reload_policy(ReloadPolicy::OnCommit)
-      .try_into()
-      .unwrap();
-
-    let searcher = reader.searcher();
+    // let searcher = reader.searcher();
+    let searcher = self.index_reader().lock().unwrap().searcher();
 
     let uuid = self.index.schema().get_field("uuid").unwrap();
     let name = self.index.schema().get_field("name").unwrap();
