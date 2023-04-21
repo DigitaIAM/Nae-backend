dbc7c7a855abd7a5288d2473c3cc5bd24cf28f70
antonovmike <antonovma@outlook.com>
2023-04-21 09:45:07 +0500

    search loop

--- src/use_cases/uc_010.rs
+++ src/use_cases/uc_010.rs
@@ -2,38 +2,54 @@ use crate::commutator::Application;
 use json::{object, JsonValue};
 use service::Services;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, self, Write};
 use std::time::Instant;
 
 const DRUGS: [&str; 1] = ["drugs"];
-const SEARCH: &str = "УТЕПЛЕННЫЙ";
+const SEARCH: &str = "ЗЕЛЕНЫЙ";
 
 pub(crate) fn import(app: &Application) {
   let items = load();
   let ctx = DRUGS.to_vec();
-
   for item in items {
     let start = Instant::now();
     crate::use_cases::csv::memories_create(app, item, ctx.clone());
     let duration = start.elapsed();
     println!("elapsed {:?}", duration);
   }
-
   app.search.write().unwrap().commit();
 }
 
 pub(crate) fn report(app: &Application) {
   let oid = "yjmgJUmDo_kn9uxVi8s9Mj9mgGRJISxRt63wT46NyTQ";
   let ctx = DRUGS.to_vec();
-  println!("CTX\t{ctx:?}");
-  let result = app
-    .service("memories")
-    .find(object! {oid: oid, ctx: ctx, search: SEARCH})
-    .unwrap();
+  // println!("CTX\t{ctx:?}");
+
+  loop {
+    print!("Текст для поиска: ");
+    io::stdout().flush().unwrap();
+    let mut search = String::new();
+    io::stdin().read_line(&mut search).unwrap();
+    let search = search.trim();
+    if search == "exit" {
+      break;
+    }
+    let result = app
+      .service("memories")
+      .find(object! {oid: oid, ctx: ctx.clone(), search: search})
+      .unwrap();
+    let json_to_string = result.dump();
+    sort_json_value(&json_to_string);
+  }
+
+  // let result = app
+  //   .service("memories")
+  //   .find(object! {oid: oid, ctx: ctx, search: SEARCH})
+  //   .unwrap();
   // println!("report:\n{}\nend of report", result.dump());
 
-  let json_to_string = result.dump();
-  println!("\tRESULT: \n{}", sort_json_value(&json_to_string));
+  // let json_to_string = result.dump();
+  // println!("\tRESULT: \n{}", sort_json_value(&json_to_string));
 }
 
 fn sort_json_value(json: &str) -> JsonValue {
@@ -46,7 +62,7 @@ fn sort_json_value(json: &str) -> JsonValue {
 }
 
 fn print_only_name(alphabet: Vec<&str>) {
-  println!("Поисковый запрос: {SEARCH}");
+  // println!("Поисковый запрос: {SEARCH}");
   let mut namevek: Vec<&str> = Vec::new();
   for i in 1..alphabet.len() {
     let name = alphabet[i].split("manufacturer").nth(0).unwrap();
