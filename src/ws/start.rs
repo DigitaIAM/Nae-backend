d198a3deb2146fb75bf2fa1c43d5cbbc54c68156
Dmitriy Shabanov <shabanovd@gmail.com>
2023-05-16 20:59:05 +0500

    cleanup

--- src/ws/start.rs
+++ src/ws/start.rs
@@ -5,7 +5,7 @@ use actix_web::{get, web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
 #[get("/")]
-pub(crate) async fn start_connection(
+pub async fn start_connection(
   req: HttpRequest,
   stream: web::Payload,
   srv: web::Data<Addr<Commutator>>,
