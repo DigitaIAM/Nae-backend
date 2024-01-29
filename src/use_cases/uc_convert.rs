5a35c5d1e0b4345c1e2cd37461532bd191b3fa2b
geopin90 <pinaev.georgy@yandex.ru>
2024-01-29 18:39:46 +0500

    uc_convert changed for labeling area

--- src/use_cases/uc_convert.rs
+++ src/use_cases/uc_convert.rs
@@ -1,25 +1,23 @@
 use crate::commutator::Application;
 use chrono::Utc;
 use csv::{ReaderBuilder, Trim, Writer};
-use json::{object, JsonValue};
+use json::object;
 use service::utils::json::JsonParams;
 use service::{Context, Services};
-use std::collections::HashMap;
 use std::fs::OpenOptions;
-use std::io::{Error, ErrorKind};
+use std::io::Error;
 use store::process_records::memories_find;
 use values::constants::{_ID, _UUID};
-use values::ID;
 
 pub fn convert_csv(app: &Application) -> Result<(), Error> {
   // range of loop hardcoded
   let mut old_date = String::from("");
   let mut global_count = 0;
 
-  for i in 5..6 {
+  for i in 0..1 {
     convert_csv_inner(
       app,
-      &format!("./data for 1C/upload/old_{i}.csv"),
+      &format!("./data for 1C/upload/labeling/old_{i}.csv"),
       &mut old_date,
       &mut global_count,
     )
@@ -38,7 +36,7 @@ pub fn convert_csv_inner(
   global_count: &mut usize,
 ) -> Result<(), Error> {
   let str_id = "yjmgJUmDo_kn9uxVi8s9Mj9mgGRJISxRt63wT46NyTQ";
-  let oid = ID::from_base64(str_id).map_err(|e| Error::new(ErrorKind::NotFound, e.to_string()))?;
+  // let oid = ID::from_base64(str_id).map_err(|e| Error::new(ErrorKind::NotFound, e.to_string()))?;
   // let ws = app.wss.get(&oid);
 
   let mut time = Utc::now().to_string();
@@ -60,18 +58,20 @@ pub fn convert_csv_inner(
   let mut headers: Vec<(String, String, String)> = Vec::new();
 
   for header in reader.headers() {
-    header.clone().iter().skip(1).for_each(|h| {
+    header.clone().iter().skip(2).for_each(|h| {
       let mut uuid = String::new();
       let mut uom = String::new();
       let header_name = h.to_lowercase();
 
-      // println!("header_name {header_name}");
+      println!("header_name {header_name}");
 
       let filter = object! {name: h};
       if let Ok(g) = memories_find(app, filter, vec!["goods"]) {
-        if header_name.starts_with("пленка") {
+        if header_name.starts_with("клей") {
           uom = String::from("кг");
-        } else if header_name.starts_with("стакан") || header_name.starts_with("крышка")
+        } else if header_name.starts_with("стакан")
+          || header_name.starts_with("полуфабрикат")
+          || header_name.starts_with("этикетка")
         {
           uom = String::from("шт");
         }
@@ -92,6 +92,16 @@ pub fn convert_csv_inner(
         });
       }
 
+      if uuid == "".to_string() {
+        uuid = if header_name.starts_with("клей") {
+          println!("1");
+          "fc7072bc-838a-4c97-9ccc-76949403a54c".to_string()
+        } else {
+          println!("2");
+          "нет".to_string()
+        };
+      }
+
       headers.push((h.to_string(), uom, uuid));
     });
   }
@@ -99,34 +109,34 @@ pub fn convert_csv_inner(
   for record in reader.records() {
     let record = record.unwrap();
 
-    let date = &record[0];
+    let date = &record[1];
 
-    // if date != *old_date {
-    *global_count += 1;
-    // }
+    if date != *old_date {
+      *global_count += 1;
+    }
 
-    for (i, r) in record.into_iter().skip(1).enumerate() {
+    for (i, r) in record.into_iter().skip(2).enumerate() {
       if r == "" {
         continue;
       }
 
       let h = headers[i].clone();
 
-      // hardcoded for thermoforming area
-      // if date == *old_date && (h.0.to_lowercase().starts_with("пленка")) {
-      //   *global_count += 1;
-      // }
+      // hardcoded for labels area
+      if date == *old_date && (h.0.to_lowercase().starts_with("стакан pure milky")) {
+        *global_count += 1;
+      }
 
       // hardcoded for concrete table
-      // if i > 1 {
-      //   wtr
-      //     .write_record([&global_count.to_string(), date, &h.0, "", r, &h.1, &h.2])
-      //     .unwrap();
-      // } else {
-      wtr
-        .write_record([&global_count.to_string(), date, "", &h.0, r, &h.1, &h.2])
-        .unwrap();
-      // }
+      if i < 1 {
+        wtr
+          .write_record([&global_count.to_string(), date, &h.0, "", r, &h.1, &h.2])
+          .unwrap();
+      } else {
+        wtr
+          .write_record([&global_count.to_string(), date, "", &h.0, r, &h.1, &h.2])
+          .unwrap();
+      }
     }
 
     *old_date = date.to_string();
